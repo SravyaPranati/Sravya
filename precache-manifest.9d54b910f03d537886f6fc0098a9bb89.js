7ba97b5d4ec4f8136b6eebf17bcf63b27c0941ea
Raja <gkrajareddy@gmail.com>
2020-04-17 02:21:22 +0530

    Updates

---
+++ precache-manifest.9d54b910f03d537886f6fc0098a9bb89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc96192a68595abecaeac67ff95b6b2",
+    "url": "/sravya/index.html"
+  },
+  {
+    "revision": "f97d653bf383abd1611a",
+    "url": "/sravya/static/css/main.bcf46ba3.chunk.css"
+  },
+  {
+    "revision": "ea09901b739fdf613b2f",
+    "url": "/sravya/static/js/2.c71ead84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sravya/static/js/2.c71ead84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97d653bf383abd1611a",
+    "url": "/sravya/static/js/main.e3d0d56a.chunk.js"
+  },
+  {
+    "revision": "b6530c9c991dd8f3e59e",
+    "url": "/sravya/static/js/runtime-main.8ca1472e.js"
+  },
+  {
+    "revision": "93a874e1adc9d7046d37c1b5b4a786cd",
+    "url": "/sravya/static/media/profile.93a874e1.jpg"
+  }
+]);
